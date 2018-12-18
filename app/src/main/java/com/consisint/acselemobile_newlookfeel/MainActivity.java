476171683d9b4cd249c90401dc248bc2e85c45fe
Alba Portillo <aportillo@consisint.com>
2018-12-18 13:42:02 -0500

    First commit

---
+++ app/src/main/java/com/consisint/acselemobile_newlookfeel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.consisint.acselemobile_newlookfeel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
